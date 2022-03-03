6620c279661db08515450a13558471d7f342466c
Crazykwak <kwj1830@naver.com>
2022-03-03 11:19:23 +0900

    Aop

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -21,23 +21,23 @@ public class MemberService {
 
     //회원가입
     public Long join(Member member){
-        //같은 이름이 있는 중복회원 XXX
-        validateDuplicateMember(member); //중복회원검증
-        memberRepository.save(member);
-        return member.getId();
+            validateDuplicateMember(member); //중복회원검증
+            memberRepository.save(member);
+            return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
-        memberRepository.findByName(member.getName())
-                .ifPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
-        });
+                    memberRepository.findByName(member.getName())
+                    .ifPresent(m -> {
+                        throw new IllegalStateException("이미 존재하는 회원입니다.");
+                    });
+
     }
 
     //회원 전체 조회회
 
    public List<Member> findMembers(){
-        return memberRepository.findAll();
+            return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberID){
